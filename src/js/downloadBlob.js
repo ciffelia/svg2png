2797aafe67a4800a05f2cdc8285860a50aa6cfae
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-11-14 10:47:07 +0900

    Update

--- src/js/downloadBlob.js
+++
@@ -1,9 +0,0 @@
-const downloadBlob = (blob, fileName) => {
-  const a = document.createElement('a')
-  a.href = URL.createObjectURL(blob)
-  a.download = fileName
-
-  a.click()
-}
-
-export default downloadBlob
